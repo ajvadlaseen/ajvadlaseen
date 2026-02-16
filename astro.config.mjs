3fb4785dd6921e42118301ba69c8df7180eff45e
Ajvad Laseen <ajvad.laseen@gmail.com>
2026-02-16 12:39:44 +0400

    feat: added 'site' in astro configuration for sitemap generation
    Temporary address

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,10 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+    site: import.meta.env.PROD
+        ? 'http://192.52.168.117:4321' // Production URL (Temporary)
+        : 'http://localhost:4321',
+
     vite: {
         plugins: [tailwindcss()],
     },
